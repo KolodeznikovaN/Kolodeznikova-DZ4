8bee2569c90c356955d08816b869b5d52631cf57
Natali <nat.kolodeznickova@yandex.ru>
2022-08-20 20:48:47 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+//Console.WriteLine("введи число А");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.Write("введи число B");
+//int b = Convert.ToInt32(Console.ReadLine());
+//int step = a;
+
+//for (int i = 1; i < b; i++)
+//{
+//step = step * a;
+//}
+//Console.WriteLine("A в степени B равно: " + step);
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+ //452 -> 11
+//82 -> 10
+//9012 -> 12
+
+//Console.WriteLine("введите число");
+//int i = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+
+//while (i > 0)
+//{
+//int num = i % 10;
+//i = i / 10;
+//sum = sum + num;
+//}
+//Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+
